d6fddd84b0f962e8c1a0f40ecf5223ffb32585f0
Divanshu7 <133848274+Divanshu7@users.noreply.github.com>
2023-10-13 14:33:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function rollDice() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    var randomImage1 = "images/dice" + randomNumber1 + ".png";
+    var randomImage1 = "dice" + randomNumber1 + ".png";
   
     var img1 = document.getElementById("imgx");
     img1.setAttribute("src", randomImage1);
